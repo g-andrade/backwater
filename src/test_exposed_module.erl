21b3fe89fa01f0267fc8e727303bf642f27ccf65
Guilherme Andrade <g@gandrade.net>
2017-07-12 23:36:52 +0100

    Add some temporary cruft

--- src/test_exposed_module.erl
+++ src/test_exposed_module.erl
@@ -3,12 +3,17 @@
 -export([function2/0]).
 -export([function3/0]).
 -export([function5/1]).
+%-export([function7/1]).
+%-export([parse_function7_json_in/1]).
+%-export([parse_function7_json_out/1]).
 
 -backwater_version("1").
 -backwater_export([{function2,0}]).
 -backwater_export({function3,0}).
 -backwater_export({function4,0}).
 -backwater_export({function5,1}).
+
+%-backwater_export({function7,1, #{ <<"application/json">> => {decode_function7_json_args, encode_function7_json_result} }}).
 
 function1() ->
     wow.
@@ -29,6 +34,15 @@ function6(Bla) ->
         1 -> rand:uniform();
         2 -> 3 / Bla
     end.
+
+%function7(Bla) ->
+%    {Bla, Bla}.
+
+%decode_function7_json_args(Value) ->
+%    jsx:decode(Value).
+%
+%encode_function7_json_result({Value1, Value2}) ->
+%    jsx:encode([Value1, Value2]).
 
 %%%
 
