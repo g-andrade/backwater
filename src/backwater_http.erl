b639550288bb08ec9589dfbf3a9bda320da85abd
Guilherme Andrade <g@gandrade.net>
2017-07-13 23:32:32 +0100

    Optimize the way response headers are used
    Lower the header names case once.

--- src/backwater_http.erl
+++ src/backwater_http.erl
@@ -23,53 +23,9 @@ encode_request(Version, Module, Function, Args, ClientConfig) ->
          {<<"content-type">>, <<MediaType/binary, "">>}],
     encode_request_with_auth(Method, Url, Headers, Body, ClientConfig).
 
-decode_response(200 = StatusCode, ResponseHeaders, ResponseBody, ClientConfig) ->
-    #{ rethrow_remote_exceptions := RethrowRemoteExceptions } = ClientConfig,
-    case decode_response_body(ResponseHeaders, ResponseBody, ClientConfig) of
-        {term, {success, ReturnValue}} ->
-            {ok, ReturnValue};
-        {term, {exception, Class, Exception, Stacktrace}} when RethrowRemoteExceptions ->
-            erlang:raise(Class, Exception, Stacktrace);
-        {term, {exception, Class, Exception, Stacktrace}} ->
-            backwater_error({remote_exception, Class, Exception, Stacktrace});
-        {raw, Binary} ->
-            backwater_error({http, StatusCode, Binary});
-        {error, {unknown_content_encoding, ContentEncoding}} ->
-            backwater_error({unknown_content_encoding, ContentEncoding});
-        {error, {undecodable_response_body, Binary}} ->
-            backwater_error({undecodable_response_body, StatusCode, Binary});
-        {error, {invalid_content_type, RawContentType}} ->
-            backwater_error({invalid_content_type, StatusCode, RawContentType})
-    end;
-decode_response(StatusCode, ResponseHeaders, ResponseBody, ClientConfig) ->
-    case decode_response_body(ResponseHeaders, ResponseBody, ClientConfig) of
-        {term, Error} ->
-            backwater_error(Error);
-        {raw, Binary} when StatusCode =:= 400 ->
-            backwater_error({bad_request, Binary});
-        {raw, Binary} when StatusCode =:= 401 ->
-            backwater_error({unauthorized, Binary});
-        {raw, Binary} when StatusCode =:= 403 ->
-            backwater_error({forbidden, Binary});
-        {raw, Binary} when StatusCode =:= 404 ->
-            backwater_error({not_found, Binary});
-        {raw, Binary} when StatusCode =:= 406 ->
-            backwater_error({not_acceptable, Binary});
-        {raw, Binary} when StatusCode =:= 413 ->
-            backwater_error({payload_too_large, Binary});
-        {raw, Binary} when StatusCode =:= 415 ->
-            backwater_error({unsupported_media_type, Binary});
-        {raw, Binary} when StatusCode =:= 500 ->
-            backwater_error({internal_error, Binary});
-        {raw, Binary} ->
-            backwater_error({http, StatusCode, Binary});
-        {error, {unknown_content_encoding, ContentEncoding}} ->
-            backwater_error({unknown_content_encoding, ContentEncoding});
-        {error, {undecodable_response_body, Binary}} ->
-            backwater_error({undecodable_response_body, StatusCode, Binary});
-        {error, {invalid_content_type, RawContentType}} ->
-            backwater_error({invalid_content_type, StatusCode, RawContentType})
-    end.
+decode_response(StatusCode, Headers, Body, ClientConfig) ->
+    CiHeaders = lists:keymap(fun backwater_util:latin1_binary_to_lower/1, 1, Headers),
+    decode_response_(StatusCode, CiHeaders, Body, ClientConfig).
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions - Requests
@@ -101,27 +57,75 @@ http_basic_auth_header_value(Username, Password) ->
 %% Internal Function Definitions - Responses
 %% ------------------------------------------------------------------
 
-decode_response_body(Headers, Body, ClientConfig) ->
-    ContentEncodingLookup = find_content_encoding(Headers),
+decode_response_(200 = StatusCode, CiHeaders, Body, ClientConfig) ->
+    #{ rethrow_remote_exceptions := RethrowRemoteExceptions } = ClientConfig,
+    case decode_response_body(CiHeaders, Body, ClientConfig) of
+        {term, {success, ReturnValue}} ->
+            {ok, ReturnValue};
+        {term, {exception, Class, Exception, Stacktrace}} when RethrowRemoteExceptions ->
+            erlang:raise(Class, Exception, Stacktrace);
+        {term, {exception, Class, Exception, Stacktrace}} ->
+            backwater_error({remote_exception, Class, Exception, Stacktrace});
+        {raw, Binary} ->
+            backwater_error({http, StatusCode, Binary});
+        {error, {unknown_content_encoding, ContentEncoding}} ->
+            backwater_error({unknown_content_encoding, ContentEncoding});
+        {error, {undecodable_response_body, Binary}} ->
+            backwater_error({undecodable_response_body, StatusCode, Binary});
+        {error, {invalid_content_type, RawContentType}} ->
+            backwater_error({invalid_content_type, StatusCode, RawContentType})
+    end;
+decode_response_(StatusCode, CiHeaders, Body, ClientConfig) ->
+    case decode_response_body(CiHeaders, Body, ClientConfig) of
+        {term, Error} ->
+            backwater_error(Error);
+        {raw, Binary} when StatusCode =:= 400 ->
+            backwater_error({bad_request, Binary});
+        {raw, Binary} when StatusCode =:= 401 ->
+            backwater_error({unauthorized, Binary});
+        {raw, Binary} when StatusCode =:= 403 ->
+            backwater_error({forbidden, Binary});
+        {raw, Binary} when StatusCode =:= 404 ->
+            backwater_error({not_found, Binary});
+        {raw, Binary} when StatusCode =:= 406 ->
+            backwater_error({not_acceptable, Binary});
+        {raw, Binary} when StatusCode =:= 413 ->
+            backwater_error({payload_too_large, Binary});
+        {raw, Binary} when StatusCode =:= 415 ->
+            backwater_error({unsupported_media_type, Binary});
+        {raw, Binary} when StatusCode =:= 500 ->
+            backwater_error({internal_error, Binary});
+        {raw, Binary} ->
+            backwater_error({http, StatusCode, Binary});
+        {error, {unknown_content_encoding, ContentEncoding}} ->
+            backwater_error({unknown_content_encoding, ContentEncoding});
+        {error, {undecodable_response_body, Binary}} ->
+            backwater_error({undecodable_response_body, StatusCode, Binary});
+        {error, {invalid_content_type, RawContentType}} ->
+            backwater_error({invalid_content_type, StatusCode, RawContentType})
+    end.
+
+decode_response_body(CiHeaders, Body, ClientConfig) ->
+    ContentEncodingLookup = find_content_encoding(CiHeaders),
     handle_response_body_content_encoding(
-      ContentEncodingLookup, Headers, Body, ClientConfig).
+      ContentEncodingLookup, CiHeaders, Body, ClientConfig).
 
-%% body encoding
+%% encoding
 
-handle_response_body_content_encoding({ok, <<"gzip">>}, Headers, Body, ClientConfig) ->
+handle_response_body_content_encoding({ok, <<"gzip">>}, CiHeaders, Body, ClientConfig) ->
     UncompressedBody = zlib:gunzip(Body), % TODO handle failure
-    ContentTypeLookup = find_content_type(Headers),
+    ContentTypeLookup = find_content_type(CiHeaders),
     handle_response_body_content_type(ContentTypeLookup, UncompressedBody, ClientConfig);
-handle_response_body_content_encoding({ok, OtherEncoding}, _Headers, _Body, _ClientConfig) ->
+handle_response_body_content_encoding({ok, OtherEncoding}, _CiHeaders, _Body, _ClientConfig) ->
     {error, {unknown_content_encoding, OtherEncoding}};
-handle_response_body_content_encoding(error, Headers, Body, ClientConfig) ->
-    ContentTypeLookup = find_content_type(Headers),
+handle_response_body_content_encoding(error, CiHeaders, Body, ClientConfig) ->
+    ContentTypeLookup = find_content_type(CiHeaders),
     handle_response_body_content_type(ContentTypeLookup, Body, ClientConfig).
 
-find_content_encoding(Headers) ->
-    find_header_value(<<"content-encoding">>, Headers).
+find_content_encoding(CiHeaders) ->
+    find_header_value(<<"content-encoding">>, CiHeaders).
 
-%% body content type
+%% content type
 
 handle_response_body_content_type({ok, {<<"application/x-erlang-etf">>, _Params}},
                                    Body, ClientConfig) ->
@@ -137,8 +141,8 @@ handle_response_body_content_type({error, content_type_missing},
                                    Body, _ClientConfig) ->
     {raw, Body}.
 
-find_content_type(Headers) ->
-    case find_header_value(<<"content-type">>, Headers) of
+find_content_type(CiHeaders) ->
+    case find_header_value(<<"content-type">>, CiHeaders) of
         {ok, ContentTypeBin} ->
             case binary:split(ContentTypeBin, [<<";">>, <<" ">>, <<$\n>>, <<$\r>>],
                               [global, trim_all])
@@ -154,22 +158,11 @@ find_content_type(Headers) ->
 
 %% utilities
 
-find_header_value(Key, Headers) ->
-    LowerKey = backwater_util:latin1_binary_to_lower(Key),
-    Predicate = fun (HeaderName) -> backwater_util:latin1_binary_to_lower(HeaderName) =:= LowerKey end,
-    case lists_keyfind_predicate(Predicate, 1, Headers) of
-        {_HeaderName, HeaderValue} -> {ok, HeaderValue};
+find_header_value(CiKey, CiHeaders) ->
+    case lists:keyfind(CiKey, 1, CiHeaders) of
+        {CiKey, Value} -> {ok, Value};
         false -> error
     end.
-
-lists_keyfind_predicate(Predicate, N, [H|T]) ->
-    Element = element(N, H),
-    case Predicate(Element) of
-        true -> H;
-        false -> lists_keyfind_predicate(Predicate, N, T)
-    end;
-lists_keyfind_predicate(_Predicate, _N, []) ->
-    false.
 
 % FIXME: duplicate in backwater_client, reconsider whole thing
 backwater_error(Error) ->
