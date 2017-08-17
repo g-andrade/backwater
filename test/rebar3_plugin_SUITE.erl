22ac6d185c26c8d7a7705dc86f10d4d098e013ce
Guilherme Andrade <g@gandrade.net>
2017-08-17 23:31:04 +0100

    Add a handful more test cases for code generation

--- test/rebar3_plugin_SUITE.erl
+++ test/rebar3_plugin_SUITE.erl
@@ -19,27 +19,149 @@ end_per_group(_Name, Config) ->
 
 %%%
 
-basic_rpc_test(_Config) ->
-    generate_and_load(
-      basic_rpc_test,
-      [{crypto, crypto, [{exports, [{version,0}]}]}]),
+single_module_function_test(_Config) ->
+    with_ref(
+      [{crypto, crypto, [{exports, [{version,0}]}]}],
+      fun () ->
+              ?assertEqual(
+                 [{version, 0}],
+                 exported_functions(rpc_crypto)),
 
-    ?assertEqual(
-       {ok, crypto:version()},
-       rpc_crypto:version()).
+              ?assertEqual(
+                 {ok, crypto:version()},
+                 rpc_crypto:version())
+      end,
+      []).
+
+whole_module_test(_Config) ->
+    with_ref(
+      [{stdlib, math, [{exports, all}]}],
+      fun () ->
+              ExportedFunctions = exported_functions(math),
+              ?assertEqual(
+                 ExportedFunctions,
+                 exported_functions(rpc_math)),
+
+              lists:foreach(
+                fun ({Function, Arity}) ->
+                        Args = [rand:uniform() || _ <- lists:seq(1, Arity)],
+                        compare_original_and_rpc_calls(math, rpc_math, Function, Args)
+                end,
+                ExportedFunctions)
+      end,
+      []).
+
+whole_module_presuming_attributes_test(_Config) ->
+    with_ref(
+      [{stdlib, math}],
+      fun () ->
+              % no attributes, so no functions are to be exported
+              ?assertEqual(
+                 [],
+                 exported_functions(rpc_math))
+      end,
+      []).
+
+current_app_module_test(_Config) ->
+    with_ref(
+      [{backwater_util, [{exports, all}]}],
+      fun () ->
+              ExportedFunctions = exported_functions(backwater_util),
+              ?assertEqual(
+                 ExportedFunctions,
+                 exported_functions(rpc_backwater_util)),
+
+              ?assertEqual(
+                 {ok, backwater_util:latin1_binary_to_lower(<<"Hello">>)},
+                 rpc_backwater_util:latin1_binary_to_lower(<<"Hello">>))
+      end,
+      [{source_directory, filename:join(source_directory(), "..")}]).
+
+current_app_module_presuming_attributes_test(_Config) ->
+    with_ref(
+      [backwater_util],
+      fun () ->
+              % no attributes, so no functions are to be exported
+              ?assertEqual(
+                 [],
+                 exported_functions(rpc_backwater_util))
+      end,
+      [{source_directory, filename:join(source_directory(), "..")}]).
+
+unloaded_application_test(_Config) ->
+    with_ref(
+      [{eldap, eldap, [{exports, all}]}],
+      fun () ->
+              ExportedFunctions = exported_functions(eldap),
+              ?assertEqual(
+                 ExportedFunctions,
+                 exported_functions(rpc_eldap)),
+
+              ?assertEqual(
+                 {ok, eldap:substrings("cn", [{any,"V"}])},
+                 rpc_eldap:substrings("cn", [{any,"V"}]))
+      end,
+      []).
+
+missing_application_test(_Config) ->
+    ?assertMatch(
+       {error, {unable_to_load_application, _}},
+       with_ref(
+         [{some_made_up, application}],
+         fun () -> error(not_supposed_to_run) end,
+         [])).
+
+missing_module_test(_Config) ->
+    ?assertMatch(
+       {error, {beam_lib, {file_error, _, enoent}}},
+       with_ref(
+         [{stdlib, some_made_up_module}],
+         fun () -> error(not_supposed_to_run) end,
+         [])).
+
+module_packed_in_escript_test(_Config) ->
+    ?assertMatch(
+       {error, {beam_lib, {file_error, _ModulePath, enotdir}}},
+       with_ref(
+         [{rebar, rebar_app_info}],
+         fun () -> error(not_supposed_to_run) end,
+         [])).
 
 %%%
 
-generate_and_load(Ref, Targets) ->
-    % code
-    Opts =
-        [{backwater_gen,
-          [{client_ref, Ref},
-           {output_directory, source_directory()}
-           | [{target, Target} || Target <- Targets]]
-         }],
-    generate_and_load_code_with_opts(Opts),
+compare_original_and_rpc_calls(Module, RpcModule, Function, Args) ->
+    Arity = length(Args),
+    ct:pal("comparing ~p vs ~p", [{Module,Function,Arity}, {RpcModule,Function,Arity}]),
+    ExpectedResult =
+        try
+            Success = apply(Module, Function, Args),
+            ct:pal("success result: ~p", [Success]),
+            {ok, Success}
+        catch
+            Class:Exception ->
+                Stacktrace = erlang:get_stacktrace(),
+                PurgedStacktrace =
+                    backwater_util:purge_stacktrace_below(
+                      {?MODULE,compare_original_and_rpc_calls,4},
+                      Stacktrace),
+                ct:pal("exception result: ~p", [{Class, Exception, PurgedStacktrace}]),
+                {error, {exception, {Class, Exception, PurgedStacktrace}}}
+        end,
+    ?assertEqual(
+       ExpectedResult,
+       apply(RpcModule, Function, Args)).
 
+with_ref(Targets, Fun, ExtraOpts) ->
+    Ref = rand:uniform(1 bsl 64),
+    try
+        backwater_util:with_success(
+          Fun,
+          generate_and_load(Ref, Targets, ExtraOpts))
+    after
+        stop(Ref)
+    end.
+
+generate_and_load(Ref, Targets, ExtraOpts) ->
     % client
     Secret = crypto:strong_rand_bytes(32),
     ok = backwater_client:start(Ref, #{ endpoint => <<"http://localhost:8080">>, secret => Secret }),
@@ -47,48 +169,101 @@ generate_and_load(Ref, Targets) ->
     % server
     ExposedModules =
         lists:map(
-          fun ({_Application, Module, TargetOpts}) ->
-                  {Module, lists_keywith([exports], 1, TargetOpts)}
+          fun ({_App, Module, TargetOpts}) ->
+                  {Module, lists_keywith([exports], 1, TargetOpts)};
+              ({App, Module}) when is_atom(App), is_atom(Module) ->
+                  Module;
+              ({Module, TargetOpts}) when is_atom(Module), is_list(TargetOpts) ->
+                  {Module, TargetOpts};
+              (Module) when is_atom(Module) ->
+                  Module
           end,
           Targets),
 
     {ok, _Pid} = backwater_server:start_clear(
-                   Ref, #{ secret => Secret, exposed_modules => ExposedModules },
-                   [{port, 8080}], #{}).
+                   Ref, #{ secret => Secret, exposed_modules => ExposedModules,
+                           return_exception_stacktraces => true },
+                   [{port, 8080}], #{}),
 
-generate_and_load_code_with_opts(OptsList) ->
-    ok = file:set_cwd(source_directory()),
-    RebarState1 = rebar_state:new(),
-    {ok, RebarAppInfo1} = rebar_app_info:new(basic_rpc_test, "0.0.0", source_directory()),
-    Opts = dict:from_list(OptsList),
-    RebarAppInfo2 = rebar_app_info:opts(RebarAppInfo1, Opts),
-    RebarState2 = rebar_state:current_app(RebarState1, RebarAppInfo2),
-    {ok, RebarState3} = backwater_rebar3_prv_generate:init(RebarState2),
-    {ok, _RebarState4} = backwater_rebar3_prv_generate:do(RebarState3),
+    SourceDirectory = proplists:get_value(source_directory, ExtraOpts, source_directory()),
+    % code
+    Opts =
+        [{backwater_gen,
+          [{client_ref, Ref},
+           {output_directory, SourceDirectory}
+           | [{target, Target} || Target <- Targets]]
+         }],
+    generate_and_load_code_with_opts(Opts, SourceDirectory).
 
+
+stop(Ref) ->
+    ok = backwater_server:stop_listener(Ref),
+    ok = backwater_client:stop(Ref).
+
+generate_and_load_code_with_opts(OptsList, SourceDirectory) ->
     {backwater_gen, BackwaterOpts} = lists:keyfind(backwater_gen, 1, OptsList),
-    TargetModules = [Module || {target, {_App, Module, _TargetOpts}} <- BackwaterOpts],
+    Targets = [Target || {target, Target} <- BackwaterOpts],
+    TargetModules = 
+        lists:map(
+          fun ({_App, Module, _TargetOpts}) ->
+                  Module;
+              ({App, Module}) when is_atom(App), is_atom(Module) ->
+                  Module;
+              ({Module, TargetOpts}) when is_atom(Module), is_list(TargetOpts) ->
+                  Module;
+              (Module) when is_atom(Module) ->
+                  Module
+          end,
+          Targets),
+
     lists:foreach(
       fun (TargetModule) ->
               TargetModuleStr = atom_to_list(TargetModule),
               ModuleStr = "rpc_" ++ TargetModuleStr,
               ModuleFilename = ModuleStr ++ ".erl",
-              Module = list_to_atom(ModuleStr),
-              %_ = file:delete(filename:join(source_directory(), ModuleFilename)),
-              %_ = file:delete(filename:join(source_directory(), ModuleStr ++ ".beam")),
-              {ok, Module} = compile:file(filename:join(source_directory(), ModuleFilename)),
-              {module, Module} = code:load_abs(filename:join(source_directory(), ModuleStr))
+              _ = file:delete(filename:join(SourceDirectory, ModuleFilename)),
+              _ = file:delete(filename:join(SourceDirectory, ModuleStr ++ ".beam"))
       end,
-      TargetModules).
+      TargetModules),
+
+    ok = file:set_cwd(SourceDirectory),
+    RebarState1 = rebar_state:new(),
+    {ok, RebarAppInfo1} = rebar_app_info:new(basic_rpc_test, "0.0.0", SourceDirectory),
+    Opts = dict:from_list(OptsList),
+    RebarAppInfo2 = rebar_app_info:opts(RebarAppInfo1, Opts),
+    RebarState2 = rebar_state:current_app(RebarState1, RebarAppInfo2),
+    {ok, RebarState3} = backwater_rebar3_prv_generate:init(RebarState2),
+    backwater_util:with_success(
+      fun (_RebarState4) ->
+              lists:foreach(
+                fun (TargetModule) ->
+                        TargetModuleStr = atom_to_list(TargetModule),
+                        ModuleStr = "rpc_" ++ TargetModuleStr,
+                        ModuleFilename = ModuleStr ++ ".erl",
+                        Module = list_to_atom(ModuleStr),
+                        {ok, Module} = compile:file(filename:join(SourceDirectory, ModuleFilename)),
+                        {module, Module} = code:load_abs(filename:join(SourceDirectory, ModuleStr))
+                end,
+                TargetModules)
+      end,
+      backwater_rebar3_prv_generate:do(RebarState3)).
 
 all_individual_tests() ->
     [Name || {Name, 1} <- exported_functions(),
              lists:suffix("_test", atom_to_list(Name))].
 
 exported_functions() ->
-    ModuleInfo = ?MODULE:module_info(),
-    {exports, Exports} = lists:keyfind(exports, 1, ModuleInfo),
-    Exports.
+    exported_functions(?MODULE).
+
+exported_functions(Module) ->
+    ModuleInfo = Module:module_info(),
+    {exports, AllExports} = lists:keyfind(exports, 1, ModuleInfo),
+    AllSortedExports = ordsets:from_list(AllExports),
+    SortedExclusions =
+        [{module_info, 0},
+         {module_info, 1}],
+    SortedExports = ordsets:subtract(AllSortedExports, SortedExclusions),
+    ordsets:to_list(SortedExports).
 
 make_dir(Path) ->
     case file:make_dir(Path) of
