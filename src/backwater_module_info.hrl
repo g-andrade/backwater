8af30a5271b865b72a2298888adedcd96e301072
Guilherme Andrade <g@gandrade.net>
2017-08-20 22:52:27 +0100

    Don't expose Elixir modules' backwater exports list
    It's a function instead of an attribute (as in regular Erlang modules)
    due to loss of custom attributes after compilation.

--- src/backwater_module_info.hrl
+++ src/backwater_module_info.hrl
@@ -1,5 +1,6 @@
 -define(METADATA_EXPORT_LIST,
-        [{behaviour_info,1}, % callbacks
-         {module_info,0},    % Erlang module info
-         {module_info,1},    % Erlang module info
-         {'__info__',1}]).   % Elixir module info
+        [{backwater_export,0}, % faux backwater export attribute in Elixir modules
+         {behaviour_info,1},   % callbacks
+         {module_info,0},      % Erlang module info
+         {module_info,1},      % Erlang module info
+         {'__info__',1}]).     % Elixir module info
