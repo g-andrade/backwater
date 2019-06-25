a7c1f3c663c2658aba3b49c3a661212d270ef87d
Alex Kovalevych <alexkovalevych@gmail.com>
2019-06-25 10:46:52 +0300

    fix decompression

--- src/backwater_encoding_gzip.erl
+++ src/backwater_encoding_gzip.erl
@@ -29,13 +29,6 @@
 -export([decode/2]).
 
 %% ------------------------------------------------------------------
-%% Macro Definitions
-%% ------------------------------------------------------------------
-
-% taken from zlib.erl at Erlang/OTP source code
--define(MAX_WBITS, 15).
-
-%% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
@@ -45,22 +38,10 @@ encode(Data) ->
 
 -spec decode(iodata(), non_neg_integer()) -> {ok, binary()} | {error, term()}.
 decode(Data, MaxUncompressedSize) ->
-    Z = zlib:open(),
-    try
-        zlib:inflateInit(Z, 16 + ?MAX_WBITS),
-        zlib:setBufSize(Z, MaxUncompressedSize),
-        case zlib:inflateChunk(Z, Data) of
-            {more, _Chunk} ->
-                {error, too_big};
-            UncompressedIoData ->
-                zlib:inflateEnd(Z),
-                UncompressedData = iolist_to_binary(UncompressedIoData),
-                true = (byte_size(UncompressedData) =< MaxUncompressedSize),
-                {ok, UncompressedData}
-        end
+    try UncompressedIoData = zlib:gunzip(Data),
+	UncompressedData = iolist_to_binary(UncompressedIoData),
+	true = byte_size(UncompressedData) =< MaxUncompressedSize,
+	{ok, UncompressedData}
     catch
-        error:Reason ->
-            {error, Reason}
-    after
-        zlib:close(Z)
+      error:Reason -> {error, Reason}
     end.
